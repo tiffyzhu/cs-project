e88552934c5128ae1bb95bc2bcbbccc603030fda
tiffyzhu <tiffyzhu@gmail.com>
2014-12-18 21:12:10 -0800

    Update World.java

--- World.java
+++ World.java
@@ -1,6 +1,31 @@
 public class World {
   
+  private Farm[] farm;
+  private int hours;
   
+  public World(Farm[] farm, int hours) {
+    this.farm = farm;
+    this.hours = hours;;
+  }
+	
+	public void setHours(double hours) {
+		this.hours = hours;
+	}
+	
+	public void setFarm(Farm[] farm) {
+	  this.farm = farm;
+	}
+	
+	public int getHours() {
+    return hours;
+  }
   
+  public void incrementHour() {
+    this.hours++;
+  }
+ 
+  public String toString() {
+    return "Hours:" + getHours() + "\n";
+  }
   
 }
